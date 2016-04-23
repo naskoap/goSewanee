b24b64e369aafb2548b4ab57f1466ad9c22fc77a
malond1 <malond0@sewanee.edu>
2016-04-23 10:40:02 -0500

    Adds preliminary html, css and php

---
+++ events.sql
@@ -0,0 +1,41 @@
+/*Sports*/
+insert into event(name, time, loc, participants)
+	values("Mens Lacrosse vs Birmingham Southern",'2016-04-22',"Harris Stadium", 80),
+	       ("Womens Lacrosse vs Berry",'2016-04-22',"The Pit", 40),
+		("Womens Tennis vs Centre ",'2016-04-22',"Tennis Court", 80),
+		("Baseball vs Covenant",'2016-04-28',"Baseball Field", 37),
+		("Mens Basketball",'2016-04-22',"Fowler Stadium", 120),
+		("Softball vs Rhodes,'2016-04-22',"Softball Field", 64),
+		("Mens Golf vs Callaway Gardens Collegiate",'2016-04-22',"The Course at Sewanee", 40),
+/*Arts
+*/insert into event(name,time,loc, particpants)
+	values("Christina Renfer Vogel:Encounters",'2016-04-29', University Art Gallery, 37),
+	      ("Sarah Lindley",'2016-08-31',University Art Gallery, 37),
+	      ("DanceWise:Dreamscapes",'2016-05-12', Tennessee Williams Center, 58),
+	      ("Hope & Prey",'2016-05-29', University Art Gallery, 30),
+	      ("ICE MUSIC",'2016-05-14',  University Art Gallery, 53),
+              ("Metamorphoses",'2016-04-29', Tennessee Williams Center, 40),
+	      ("My America TOO",'2016-04-29',  Convocation Hall, 21),
+
+/*Community
+*/insert into event(name,time,loc, particpants)
+	values("Sewaneroo",'2016-04-29', Lake Cheston, 25),
+	      ("LED Conversation",'2016-08-31',Social Lodge, 37),
+	      ("Sips & Saplings",'2016-05-12', Snowden, 58),
+	      ("Phi Sgma Car Wash",'2016-05-29', Bishops Common, 30),
+	      ("Garden Meditation",'2016-05-14',  Greenhouse, 53),
+              ("Environmental Racism,'2016-04-29', Gailor Auditorium, 40),
+	      ("Rain Barrel",'2016-04-29',  Green House, 21),
+
+/*Religious
+*/insert into event(name,time,loc, particpants)
+	values("Sewaneroo",'2016-04-29', Lake Cheston, 25),
+	      ("LED Conversation",'2016-08-31',Social Lodge, 37),
+	      ("Meditation & Prayer",'2016-05-12', Tennessee Williams Center, 58),
+	      ("Hope & Prey",'2016-05-29', University Art Gallery, 30),
+	      ("ICE MUSIC",'2016-05-14',  University Art Gallery, 53),
+              ("Metamorphoses",'2016-04-29', Tennessee Williams Center, 40),
+	      ("My America TOO",'2016-04-29',  Convocation Hall, 21),
+
+
+		
---
+++ loginform.php
@@ -0,0 +1,23 @@
+<!--
+  Divine Maloney
+  Grayson Ruhl
+  Nasko Apostolov
+
+  loginform.html - This is the main file for our project and holds all of our html 
+  for our main web page.
+-->
+<html>
+<link rel="stylesheet" type="text/css" href="goSewanee.css">
+<body>
+	<form action="loginPageMockUp.html">
+  		Username:<br>
+  		<input type="text" name="firstname" value="">
+  		<br>
+  		Password:<br>
+  		<input type="text" name="lastname" value="">
+  		<br><br>
+  		<input type="submit" value="Submit">
+	</form>
+
+</body>
+</html>
