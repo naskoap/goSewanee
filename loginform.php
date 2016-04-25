400c5f6fb81dc6845a4274b081544fad2309a33b
ruhlgl <graysonruhl@gmail.com>
2016-04-24 21:09:58 -0500

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -11,13 +11,35 @@
 <body>
 	<form action="loginPageMockUp.html">
   		Username:<br>
-  		<input type="text" name="firstname" value="">
+  		<input type="text" name="username_check" value="">
   		<br>
   		Password:<br>
-  		<input type="text" name="lastname" value="">
+  		<input type="text" name="password_check" value="">
   		<br><br>
   		<input type="submit" value="Submit">
 	</form>
+	
+<?php
+
+	$username = "SELECT username FROM users WHERE username = username_check";
+	$password = "SELECT password FROM users WHERE password = username_check"
+	$UserPassValid = "Your username and password exist in our database."
+	$userPassError = "We cannot recognize your username and or password. Please try again."
+
+	if($username == $username_check)
+		$check = 1;
+	else
+		$check = 0;
+		
+	if($username == $username_check)
+		$check++;
+		
+	if($check == 1)
+		echo $UserPassValid;
+	else
+		echo $UserPassError;
+
+?>
 
 </body>
 </html>
